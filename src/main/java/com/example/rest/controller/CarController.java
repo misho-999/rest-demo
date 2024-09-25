594f3c7dac67eb92ab805548bfb86eb689193f0c
Angelov <mihail.angelov@accenture.com>
2024-09-25 21:37:45 +0300

    Add custom metric for counting getAllCars() requests

--- src/main/java/com/example/rest/controller/CarController.java
+++ src/main/java/com/example/rest/controller/CarController.java
@@ -2,6 +2,8 @@ package com.example.rest.controller;
 
 import com.example.rest.model.Car;
 import com.example.rest.service.CarService;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +14,31 @@ import java.util.List;
 @RestController
 @RequestMapping("cars")
 public class CarController {
+    //Related to Actuator
+    private final DistributionSummary summary;
+    //Related to Actuator. Count the number of calls to getAllCars();
+    private Double count = 0.0;
 
     private final CarService carService;
 
-    public CarController(CarService carService) {
+       /**
+     * We Create custom metric in Spring Actuator.
+     * We call this metric using: /actuator/metrics/cars.summary endpoint
+     * @param meter It comes from spring actuator
+     * @param carService Injected service
+     */
+    public CarController(MeterRegistry meter, CarService carService) {
         this.carService = carService;
+        summary = DistributionSummary.builder("cars.summary")  //Build a custom metric (cars.summary)  and register it
+                .baseUnit("times")
+                .register(meter);
+
     }
 
     @GetMapping("/all")
-    private ResponseEntity<List<Car>> getAllCars(){
+    private ResponseEntity<List<Car>> getAllCars() {
+        summary.record(++count);
+
         return ResponseEntity
                 .ok()
                 .body(carService.findAllCars());
