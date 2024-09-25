98a0f9c785f4ff957996bcfe2b8c66b7c01312c1
Angelov <mihail.angelov@accenture.com>
2024-09-25 15:47:20 +0300

    Added InMemoryUserDetailsManagerConfig

--- src/main/java/com/example/rest/config/SecurityConfig.java
+++ src/main/java/com/example/rest/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class SecurityConfig {
 
@@ -14,7 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authz) -> authz
                         .requestMatchers(HttpMethod.GET, "/users/all").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/cars/all").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/cars/all").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET, "/users/*").permitAll()
 
                         .requestMatchers(HttpMethod.PUT, "/users/*").permitAll()
@@ -24,6 +26,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.DELETE, "/users/{id}").permitAll()
 
                         .anyRequest().authenticated())
+                .httpBasic(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
