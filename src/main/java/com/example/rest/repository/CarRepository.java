ae1007be4df47270d2aa5a28c7ed5bf5f02398f5
Angelov <mihail.angelov@accenture.com>
2024-10-07 17:56:29 +0300

    Added postman requests

--- src/main/java/com/example/rest/repository/CarRepository.java
+++ src/main/java/com/example/rest/repository/CarRepository.java
@@ -1,15 +1,9 @@
 package com.example.rest.repository;
 
 import com.example.rest.model.Car;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
