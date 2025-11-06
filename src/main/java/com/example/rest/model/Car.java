7136af5d3e7e0d08d9744a2593341423b37c6ac8
misho-999 <misho_angelov@yahoo.com>
2025-11-06 17:41:09 +0200

    Added unit test

--- src/main/java/com/example/rest/model/Car.java
+++ src/main/java/com/example/rest/model/Car.java
@@ -1,13 +1,17 @@
 package com.example.rest.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "cars")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/example/rest/service/impl/CarServiceImplTest.java
+++ src/test/java/com/example/rest/service/impl/CarServiceImplTest.java
@@ -1,18 +1,42 @@
 package com.example.rest.service.impl;
 
+import com.example.rest.model.Car;
 import com.example.rest.repository.CarRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class CarServiceImplTest {
 
     @Mock
     private CarRepository carRepository;
 
+    //The class whose methods we are testing
+    @InjectMocks
+    CarServiceImpl carServiceImpl;
+
     @Test
     void findAllCars() {
+        Car car1 = new Car(1, "Toyota", "Corolla", null);
+        Car car2 = new Car(2, "Honda", "Civic", null);
+
+        when(carRepository.findAll()).thenReturn(List.of(car1, car2));
+
+        List<Car> allCars = carServiceImpl.findAllCars();
+
+        verify(carRepository, times(1)).findAll();
+
+        assertEquals(2, allCars.size());
+        assertEquals("Toyota", allCars.get(0).getMake());
+        assertEquals("Honda", allCars.get(1).getMake());
     }
 
     @Test
